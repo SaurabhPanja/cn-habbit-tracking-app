50720a673af213bd5c233f92d77d00e4010ff13c
Saurabh Panja <saurabh.panja@phamax.ch>
2023-05-15 21:06:33 +0530

    can add habbits

--- src/Habbits.js
+++ src/Habbits.js
@@ -1,22 +1,31 @@
-import React, { useContext } from 'react'
-import { ReactReduxContext } from 'react-redux'
+import React, { useContext, useState } from 'react'
+import { ReactReduxContext, connect } from 'react-redux'
 
-export default function Habbits() {
+const Habbits = () =>{
   const { store } = useContext(ReactReduxContext)
+  const [newHabbit, setNewHabbit] = useState("")
+  const [appState, setAppState] = useState(store.getState())
 
-  console.log(store.getState())
+  const habbits = appState['habbits']
+  
+  const handleOnClick = (e)=> {
+    if(newHabbit.length > 0)
+      store.dispatch({ type: 'ADD_HABBIT', payload: newHabbit })
+      setAppState(store.getState())
+      setNewHabbit("")
+  }
+
+
   return (
     <div>
-        <div>
-            <a href="/routine">Habbit 1</a>
-        </div>
-        <div>
-            Habbit 2
-        </div>
-        <div>
-            Habbit 3
-        </div>
-        <button>Add +</button>
+      {habbits.map((habbit, index)=>(
+        <div key={index}>{habbit.text}</div>
+      ))}
+        <input type='text' onChange={(e)=>setNewHabbit(e.target.value)} value={newHabbit}/>
+        <button onClick={handleOnClick}>Add +</button>
     </div>
   )
 }
+
+export default connect()(Habbits);
+
--- src/reducers.js
+++ src/reducers.js
@@ -1,15 +1,70 @@
 const initialState = {
   habbits: [
-    { id: 0, text: 'Swimming', status: "none" },
-    { id: 1, text: 'Jogging', status: "not-done"},
-    { id: 2, text: 'Gymn', status: "done"}
+    { id: 0, text: 'Swimming', status: {
+        "day_1": "none",
+        "day_2": "none",
+        "day_3": "none",
+        "day_4": "none",
+        "day_5": "none",
+        "day_6": "none",
+        "day_7": "none"
+      }
+    },
+    { id: 1, text: 'Jogging', status: {
+        "day_1": "none",
+        "day_2": "none",
+        "day_3": "none",
+        "day_4": "none",
+        "day_5": "none",
+        "day_6": "none",
+        "day_7": "none"
+      }
+    },
+    { id: 2, text: 'Gymn', status: {
+        "day_1": "none",
+        "day_2": "none",
+        "day_3": "none",
+        "day_4": "none",
+        "day_5": "none",
+        "day_6": "none",
+        "day_7": "none"
+      }
+    }
   ]
+}
+
+function nextHabbitId(todos) {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+  return maxId + 1
 }
 
 export default function rootReducer(state = initialState, action) {
     switch (action.type) {
       case 'ADD_HABBIT':
-        return {...state, }
+        return {
+          // that has all the existing state data
+          ...state,
+          // but has a new array for the `habbits` field
+          habbits: [
+            // with all of the old habbits
+            ...state.habbits,
+            // and the new todo object
+            {
+              // Use an auto-incrementing numeric ID
+              id: nextHabbitId(state.habbits),
+              text: action.payload,
+              status: {
+                "day_1": "none",
+                "day_2": "none",
+                "day_3": "none",
+                "day_4": "none",
+                "day_5": "none",
+                "day_6": "none",
+                "day_7": "none"
+              }
+            }
+          ]
+        }
       default:
         return state
     }
